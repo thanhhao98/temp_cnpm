ac11902aef9a78ccb45ee92399115306ee43762b
ThanhHao <hao.phanthanh98@gmail.com>
2019-04-02 13:28:19 +0700

    fix index for run

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+// Constants
+const PORT     = process.env.PORT || 8080;
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>\n');
+});
+
+app.listen(PORT);
+console.log(`Running on http://localhost:${PORT}`);
