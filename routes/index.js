78796dcb91796bd9db721e63b5afddf4e63abebe
ThanhHao <hao.phanthanh98@gmail.com>
2019-04-13 18:00:37 +0700

    un backup database

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 
+
+router.get('/',(req,res)=>{
+    res.status(200).send('<h1>hello woradfsadfasdf</h1>')
+})
 router.get('/api/v1/todos', (req, res) => {
     data = {
         "Search": [
